e677785a3f1b10067c7ed2e2816d3cd0f4327721
nikkbh <nikhilbhutani111@yahoo.com>
2026-02-05 22:08:06 +0530

    feat: add azure_object_id variable for OIDC authentication in Terraform

--- infra/terraform/vnet-deployment/variables.tf
+++ infra/terraform/vnet-deployment/variables.tf
@@ -47,3 +47,8 @@ variable "oidc_token_file_path" {
   type        = string
   default     = ""
 }
+
+variable "azure_object_id" {
+  description = "Azure Object ID for OIDC authentication"
+  type        = string
+}
