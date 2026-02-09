bc1fd29829d2ef2533d6ad5c1a6968395a233b18
nikkbh <nikhilbhutani111@yahoo.com>
2026-02-09 20:55:24 +0530

    feat: add public ip to NIC

--- infra/terraform/modules/network-interface-card/main.tf
+++ infra/terraform/modules/network-interface-card/main.tf
@@ -1,3 +1,10 @@
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.name}-pip"
+  resource_group_name = var.rg
+  location            = var.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
 resource "azurerm_network_interface" "nic" {
   name                = var.name
   resource_group_name = var.rg
@@ -7,5 +14,6 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
